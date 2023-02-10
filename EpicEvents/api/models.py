b0b6ed9d031968cad09e2604b831a1f9cf7b8547
Jennifer Cotte <jennifer.malacain@laposte.net>
2023-02-10 18:35:41 +0100

    Fonctionnalité 3 ok: implémentation des modèles

--- EpicEvents/api/models.py
+++ EpicEvents/api/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Client(models.Model):
+    class Statut(models.TextChoices):
+        Prospect = "Prospect"
+        Client = "Client"
+
+    nom = models.CharField(max_length=25)
+    prenom = models.CharField(max_length=25)
+    email = models.EmailField()
+    tel = models.CharField(max_length=20)
+    port = models.CharField(max_length=20)
+    societe = models.CharField(max_length=250)
+    statut = models.CharField(max_length=10, choices=Statut.choices)
+    date_creation = models.DateField(auto_now_add=True)
+    date_maj = models.DateField(auto_now=True)
+    vendeur = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL)
+
+
+class Contrat(models.Model):
+    ouvert = models.BooleanField(default=True)
+    signe = models.BooleanField(default=False)
+    date_signature = models.DateField()
+    montant = models.FloatField()
+    echeance = models.DateField()
+    date_creation = models.DateField(auto_now_add=True)
+    date_maj = models.DateField(auto_now=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+
+
+class Evenement(models.Model):
+    ouvert = models.BooleanField(default=True)
+    date_evenement = models.DateField()
+    participants = models.PositiveIntegerField()
+    notes = models.TextField()
+    date_creation = models.DateField(auto_now_add=True)
+    date_maj = models.DateField(auto_now=True)
+    contrat = models.OneToOneField(Contrat, on_delete=models.CASCADE)
+    support = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL)
--- EpicEvents/connexion/models.py
+++ EpicEvents/connexion/models.py
@@ -3,9 +3,24 @@ from django.contrib.auth.models import AbstractUser
 
 
 class Personnel(AbstractUser):
+    GESTION = "GESTION"
+    VENTE = "VENTE"
+    SUPPORT = "SUPPORT"
+
+    CHOIX = (
+        (GESTION, "Gestion"),
+        (VENTE, "Vente"),
+        (SUPPORT, "Support")
+    )
+
     first_name = None
     last_name = None
     nom = models.CharField(max_length=25)
     prenom = models.CharField(max_length=25)
     tel = models.CharField(max_length=20)
     port = models.CharField(max_length=20)
+    equipe = models.CharField(max_length=10, choices=CHOIX)
+    email = models.EmailField(unique=True)
+    username = None
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
