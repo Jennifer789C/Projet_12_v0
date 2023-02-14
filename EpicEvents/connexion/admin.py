70774a4296fc21caa3c100f9ba257d3221e72d6f
Jennifer Cotte <jennifer.malacain@laposte.net>
2023-02-14 20:38:19 +0100

    creation user depuis admin ok mais la sélection du groupe gestion n'active pas l'option is_staff

--- EpicEvents/connexion/admin.py
+++ EpicEvents/connexion/admin.py
@@ -1,9 +1,39 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Personnel
 
 
-class PersonnelAdmin(admin.ModelAdmin):
+class CreationUserForm(forms.ModelForm):
+    class Meta:
+        model = Personnel
+        fields = ("email", "password")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+
+class PersonnelAdmin(UserAdmin):
+    add_form = CreationUserForm
+
+    ordering = ("id", )
     list_display = ("id", "nom", "prenom", "email", "equipe")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Informations personnelles", {"fields": ("nom", "prenom", "tel", "port")}),
+        ("Autorisations", {"fields": ("equipe", "is_active", "is_staff")}),
+        ("Dates", {"fields": ("date_joined", "last_login")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password"),
+        }),
+    )
 
 
 admin.site.register(Personnel, PersonnelAdmin)
--- EpicEvents/connexion/models.py
+++ EpicEvents/connexion/models.py
@@ -1,5 +1,32 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, Group
+from django.contrib.auth.models import BaseUserManager, AbstractUser, Group
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("Veuillez saisir une adresse mail")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Un superuser doit avoir l'option 'is_staff' activé")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Un superuser doit avoir l'option 'is_superuser' activé")
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class Personnel(AbstractUser):
@@ -18,12 +45,14 @@ class Personnel(AbstractUser):
     nom = models.CharField(max_length=25)
     prenom = models.CharField(max_length=25)
     tel = models.CharField(max_length=20)
-    port = models.CharField(max_length=20)
+    port = models.CharField(max_length=20, blank=True, null=True)
     equipe = models.CharField(max_length=10, choices=CHOIX)
     email = models.EmailField(unique=True)
     username = None
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     def __str__(self):
         return self.prenom
